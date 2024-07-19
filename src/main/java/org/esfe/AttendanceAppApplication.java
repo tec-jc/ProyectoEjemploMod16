57b86d19cf871b543ab90c2cc229736321816b27
JC Tula <jc_tula@outlook.com>
2024-07-19 13:49:41 -0600

    estructura del proyecto y configuración de layout

---
+++ src/main/java/org/esfe/AttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceAppApplication.class, args);
+	}
+
+}
